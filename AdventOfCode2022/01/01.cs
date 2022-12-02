4951a4e9b5d4af9bc05af8fe29160a842c93c150
Oliver Johnston <oliver@ojohnston.com>
2022-12-02 20:37:35 +0000

    day 1

---
+++ AdventOfCode2022/01/01.cs
@@ -0,0 +1,23 @@
+﻿using OliverJ.AdventOfCode2022.Utils;
+
+namespace OliverJ.AdventOfCode2022._01;
+
+class _01 : AocBase
+{
+    protected override object Part1(string input)
+    {
+        return input.Split("\r\n\r\n")
+            .Select(g => g.Split("\r\n").Sum(int.Parse))
+            .Max();
+    }
+
+    protected override object Part2(string input)
+    {
+        return input.Split("\r\n\r\n")
+            .Select(g => g.Split("\r\n").Sum(int.Parse))
+            .OrderByDescending(x => x)
+            .Take(3)
+            .Sum();
+    }
+}
+
