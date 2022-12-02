b61b4726649b15cdf827cd103e23a9dd6f48fcbf
Oliver Johnston <oliver@ojohnston.com>
2022-12-02 21:04:33 +0000

    Update 01.cs

--- AdventOfCode2022/01/01.cs
+++ AdventOfCode2022/01/01.cs
@@ -7,14 +7,14 @@ class _01 : AocBase
     protected override object Part1(Input input)
     {
         return input.LineGroups()
-            .Select(line => line.Sum(int.Parse))
+            .Select(g => g.Sum(int.Parse))
             .Max();
     }
 
     protected override object Part2(Input input)
     {
         return input.LineGroups()
-            .Select(line => line.Sum(int.Parse))
+            .Select(group => group.Sum(int.Parse))
             .OrderByDescending(x => x)
             .Take(3)
             .Sum();
